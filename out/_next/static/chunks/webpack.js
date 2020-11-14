f968c3e04249bb937c70a0ad2f98673c3e4ee4bc
Max Jackson <modulations@gmail.com>
2020-11-14 17:06:11 -0500

    new deploy with header fix

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0044910545f26889d03c";
+/******/ 	var hotCurrentHash = "c23a238b5bef8cd1e483";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

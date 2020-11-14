ece6ff1290c1eceab4d26af92152d4abe6c8ea14
Max Jackson <modulations@gmail.com>
2020-11-14 17:31:02 -0500

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c23a238b5bef8cd1e483";
+/******/ 	var hotCurrentHash = "1c368815e1205cbebef8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

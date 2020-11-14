abb22efaf4dbafd64d7bcdc054414694432b2cf7
Max Jackson <modulations@gmail.com>
2020-11-14 16:11:01 -0500

    adding mobile scaling to head

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import Head from 'next/head'
 import Hero from '../components/Hero'
 import { CodeSection } from '../components/Sections/Code'
 import { SocialSection } from '../components/Sections/Social'
@@ -12,6 +13,9 @@ import theme from '../themes.js';
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Hero theme={theme.dark}/>
       <CodeSection />
       <NFTSection />

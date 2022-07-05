7d850afd9d0196087a75d13194c5d493dab78eb2
MXJXN <modulations@gmail.com>
2022-07-05 16:38:38 -0400

    restart

--- out/_next/static/JbXNkTwDuF4xw3z-SkAHC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-619649101b31a94f7cc5.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f4f567df00ce0843964e.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
 import styled from 'styled-components'
 import Head from 'next/head'
-import Hero from '../components/Hero'
-import { CodeSection } from '../components/Sections/Code'
-import { SocialSection } from '../components/Sections/Social'
-import { NFTSection } from '../components/Sections/Art'
-import { MusicSection } from '../components/Sections/Music'
-import { Footer } from '../components/Sections/Footer'
-import theme from '../themes.js';
 
 
 
@@ -16,12 +9,6 @@ export default function Home() {
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Hero theme={theme.dark}/>
-      <CodeSection />
-      <NFTSection />
-      <MusicSection />
-      <SocialSection />
-      <Footer />
     </>
   )
 }
